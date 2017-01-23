323d21574c14ecfa396cb94daa7b557a268cab35
Erlend Eriksen <erlend_ne@hotmail.com>
2017-01-23 20:21:57 +0100

    moderate getTup

--- scripts/getTup.sh
+++ scripts/getTup.sh
@@ -7,5 +7,5 @@ git reset --hard 314ed32d64a7dec9986854b8ac97780ed9ce64f7
 sudo cp build/tup /usr/bin/
 cd ..
 rm -rf tup
-# The following line is needed for some linux destributions, including debian jessie
+# The following line may be needed in special cases
 #sudo chmod u+s /usr/bin/tup
