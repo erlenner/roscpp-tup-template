03ef380d5a0e298979158ec44785c58683b59991
Erlend Eriksen <erlend_ne@hotmail.com>
2017-01-23 02:13:07 +0100

    set tup version fixed at 0.7.5

--- scripts/getTup.sh
+++ scripts/getTup.sh
@@ -2,6 +2,7 @@
 sudo apt-get install -y libfuse-dev
 git clone https://github.com/gittup/tup.git
 cd tup
+git reset --hard 314ed32d64a7dec9986854b8ac97780ed9ce64f7
 ./bootstrap.sh
 sudo cp build/tup /usr/bin/
 cd ..
